48a1b09b2e0047b02212866f146845b499847cda
rishipandey2 <rishipandeyinfo@gmail.com>
2025-01-20 11:20:50 +0530

    New Projec Added

---
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.notes
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.notes.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addButton.setOnClickListener{
+            val intent = Intent(this, AddNoteActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
